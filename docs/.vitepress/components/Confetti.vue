c2a0aa0a3781c2070e97d69d88fb93092756effa
simple <ProsperousEnding@Gmail.com>
2024-12-31 01:53:46 +0800

    fix: Confetti

--- docs/.vitepress/components/Confetti.vue
+++ docs/.vitepress/components/Confetti.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import confetti from "canvas-confetti";
+import { onMounted } from 'vue'
 
-/* 纸屑 */
-confetti({
-  particleCount: 100,
-  spread: 170,
-  origin: { y: 0.6 },
-});
+onMounted(async () => {
+  const confetti = (await import('canvas-confetti')).default
+  confetti({
+    particleCount: 100,
+    spread: 170,
+    origin: { y: 0.6 },
+  })
+})
 </script>
